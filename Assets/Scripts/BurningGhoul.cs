11a6b70317da05ce75b33bbf2468fc1778fbf92d
Miguel Pelaez Duro <mpelaezd02@educastillalamancha.es>
2024-02-21 18:36:13 +0100

    21/02/2024
    Burning Ghoul pegable

---
+++ Assets/Scripts/BurningGhoul.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurningGhoul : MonoBehaviour
+{
+    public float velocidad; //Velocidad de movimiento
+    public Vector3 posicionFin; //Posición a la que queremos que se desplace
+    public Vector3 posicionInicio; //Posición actual
+    private bool moviendoAFin; //Para saber si vamos en dirección a la posición final o ya estamos de vuelta
+
+    //Variables de la barra de vida
+    public float health = 100;
+    public float maxHealth = 100;
+    public FloatingHealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionInicio = transform.position;  //Nos da la posición en la que estamos
+
+        moviendoAFin = true;
+
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+        healthBar.UpdateHealthBar(health, maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverEnemigo();
+    }
+    private void MoverEnemigo()
+    {
+        Vector3 posiciondestino = (moviendoAFin) ? posicionFin : posicionInicio;
+        transform.position = Vector3.MoveTowards(transform.position, posiciondestino, velocidad * Time.deltaTime);
+        if (transform.position == posicionFin) moviendoAFin = false;
+        if (transform.position == posicionInicio) moviendoAFin = true;
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        healthBar.UpdateHealthBar(health, maxHealth);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
