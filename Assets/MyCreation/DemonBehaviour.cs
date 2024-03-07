2648ec87ccface5c9b6c5ad7800f24e71fe6ebe9
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2024-03-07 13:49:03 +0100

    07/03/2024 13:38
    TENEMOS JOYSTICK CHAVALES ME HAGO PAJAS

--- Assets/MyCreation/DemonBehaviour.cs
+++ Assets/MyCreation/DemonBehaviour.cs
@@ -9,6 +9,11 @@ public class DemonBehaviour : MonoBehaviour
     Rigidbody2D rb2d;
     SpriteRenderer spRd;
 
+    // Para el movimiento del jugador con joystick
+    public Joystick joystick;
+    private float movimientoV;
+    private float movimientoH;
+
     //Salto de jugador
     //Para averiguar si esta saltando, asi se comprueba que no se pueda saltar varias veces en el aire
     bool isJumping = false;
@@ -32,14 +37,29 @@ public class DemonBehaviour : MonoBehaviour
     {
         //GRAVEDAD PARA QUE EL SALTO NO PAREZCA QUE ESTA EN LA LUNA
         rb2d.AddForce(new Vector2(0f,-1f));
+
+
         //RECOJO LOS VALORES QUE INDICAN EL MOVIMIENTO DEL PERSONAJE (1) - derecha , (-1) - izquierda, (0) - parado
-        float movimientoH = Input.GetAxisRaw("Horizontal");
+        // inclusión del joystick; si los valores de movimiento superan una cierta sensibilidad, el movimiento horizontal
+        // se pilla del joystick; si no, de los botones
+        if (joystick.Horizontal >  0f | joystick.Horizontal < 0f)
+        {
+            movimientoH = joystick.Horizontal;
+        }
+        else
+        {
+            movimientoH = Input.GetAxisRaw("Horizontal");
+        }
+        
+
+
         rb2d.velocity = new Vector2(movimientoH * velocidad, rb2d.velocity.y);
 
         //Esto indica al Rigidbody2D la velocidad que queremos que tenga
 
         //EJE X : MovimientoH: para indicar la direccion del movimiento.
         //EJE Y : Obtengo la que tenia antes mediante rb2d.velocity.y
+
 
         if (movimientoH > 0)
         {
@@ -51,13 +71,22 @@ public class DemonBehaviour : MonoBehaviour
             dir = true;
             spRd.flipX = true;
         }
+        
 
-        if (Input.GetButton("Jump") && !isJumping)
+
+
+        movimientoV = joystick.Vertical;
+        if ((Input.GetButton("Jump") && !isJumping) || (movimientoV >= 0.5f && !isJumping))
         {
             rb2d.AddForce(Vector2.up * potenciaSalto);
             animator.SetBool("isJumping", true);
             isJumping = true;
+        } else
+        {
+            movimientoV = 0f;
         }
+
+
 
         if (movimientoH != 0)
         {
